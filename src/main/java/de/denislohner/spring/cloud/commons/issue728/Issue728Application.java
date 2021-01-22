16b6b1c92742097f46dabadab9e206d12ffd8537
Denis Lohner <mail@denislohner.de>
2021-01-22 21:51:44 +0100

    initial commit

---
+++ src/main/java/de/denislohner/spring/cloud/commons/issue728/Issue728Application.java
@@ -0,0 +1,13 @@
+package de.denislohner.spring.cloud.commons.issue728;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Issue728Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Issue728Application.class, args);
+	}
+
+}
---
+++ src/test/java/de/denislohner/spring/cloud/commons/issue728/Issue728ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.denislohner.spring.cloud.commons.issue728;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Issue728ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
